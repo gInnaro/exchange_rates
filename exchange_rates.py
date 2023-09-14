2c62164ac5ccc872d049689f2da7c4211b164312
gInnaro <106085169+gInnaro@users.noreply.github.com>
2023-09-14 15:52:07 +0300

    Add files via upload

---
+++ exchange_rates.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import requests
+ 
+app = FastAPI()
+
+@app.get("/api/rates")
+def daily_rates(froms: str, to: str, value: float):
+    r = requests.get('https://www.cbr-xml-daily.ru/latest.js').json()
+    if to == 'RUB':
+        data = (1 / r['rates'][froms]) * value
+        return {"result": data}
+    elif froms == 'RUB':
+        data = r['rates'][to] * value
+        return {"result": data}
+    else:
+        return {"result": 'Выберите отношение к RUB'}
+        
